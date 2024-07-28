f0b33ee6595c16a27566002c572835425655ae1c
Dmitrii Rychkov <dmitrii@rychkov.pro>
2024-07-28 14:34:15 +0500

    add factorial.spec

--- specs/factorial.spec.js
+++ specs/factorial.spec.js
@@ -11,4 +11,20 @@ describe ('Test of factorial function', () => {
         const result = factorial(1); /// ACT
         expect(result).toBe(1); /// ASSERT
     });
+    it ('factorial(3) is 6', () => {
+        const result = factorial(3); /// ACT
+        expect(result).toBe(6); /// ASSERT
+    });
+    it ('factorial(-1) fails', () => {
+        const expression = () => factorial(-1); /// ACT
+        expect(expression).toThrow('range'); /// ASSERT
+    });
+    it ('factorial(101) fails', () => {
+        const expression = () => factorial(101); /// ACT
+        expect(expression).toThrow('range'); /// ASSERT
+    });
+    it ('factorial("abc") fails', () => {
+        const expression = () => factorial('abc'); /// ACT
+        expect(expression).toThrow('X must be number'); /// ASSERT
+    });
 });
