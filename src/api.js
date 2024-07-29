52b0a6bd1ecf6fa8783fad53f8279577b98c84d9
Dmitrii Rychkov <dmitrii@rychkov.pro>
2024-07-29 12:18:45 +0500

    api

---
+++ src/api.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+
+let url = 'https://bookstore.demoqa.com/Account/v1/User';
+
+let options = {
+  method: 'POST',
+  headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/9.3.2'},
+  body: '{"userName":"your_username7","password":"Your_Password123!"}'
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error('error:' + err));
